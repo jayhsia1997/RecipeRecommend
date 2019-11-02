93ce0489389d77129e590c0a0153a1b56e73cde5
cyc5216 <cia20041@hotmail.com>
2019-11-02 13:08:54 +0800

    Add new branch'Joey_branch'.

--- Joey/crawler_cook1cook.py
+++ Joey/crawler_cook1cook.py
@@ -1,6 +1,7 @@
 import requests, bs4, re, os, jieba, datetime, time, random
 
-###建立資料夾
+
+### 建立資料夾
 now_date = str(datetime.date.today())
 folder_path = r"./downloads/COOK1COOK_" + now_date
 if os.path.exists(folder_path)  == False:
@@ -19,17 +20,31 @@ for i in range(1):
         recipe_url = start_page + str(recipe_num)
         print("Recipe downloading: " + recipe_url)
 
-        ###取得HTML
+        ### 取得HTML
         recipe_searched_html = requests.get(url = recipe_url, headers = my_headers)  #欲下載網址網址當參數，傳回網頁的HTML格式
         recipe_searched_html.raise_for_status()  #.raise_for_status()若目標網頁伺服器阻擋，擷取望頁內容有錯誤時，列印出原因
         recipe_searched_soup = bs4.BeautifulSoup(recipe_searched_html.text, "html.parser")  #.text是指網頁內容，lxml是較新的、解析HTML文件的方式
 
-        ###料理名
+        ### 料理名
         dish = recipe_searched_soup.select('h1[itemprop="name"]')[0]
         dish_name = dish.text.strip()
         print(dish_name)
 
-        ###食材
+        ### 日期
+        upload_date = recipe_searched_soup.select('div[class="col-xs-12"] span')[0].text
+        upload_date = re.sub(r'[^0-9]*', "", upload_date)
+        upload_date = datetime.datetime.strptime(upload_date, "%Y%m%d").strftime("%Y-%m-%d")
+        print(upload_date)
+
+        ### 份數
+        try:
+            serving = recipe_searched_soup.select('span[itemprop = "recipeYield"]')[0].text
+            serving = re.sub(r"[^0-9]*", "", serving)[0]
+            print(serving)
+        except:
+            pass
+
+        ### 食材
         ingredients =  recipe_searched_soup.select('span[class="pull-left ingredient-name"]')
         ingredients_list = []
         for i in ingredients:
@@ -37,7 +52,7 @@ for i in range(1):
         ingredients_str = ",".join(ingredients_list)
         #print(ingredients_str)
 
-        ###數量單位
+        ### 數量單位
         quantities = recipe_searched_soup.select('span[class="pull-right ingredient-unit"]')
         quantities_list = []
         for i in quantities:
@@ -45,7 +60,7 @@ for i in range(1):
         quantities_str = ",".join(quantities_list)
         #print(quantities_str)
 
-        ###料理步驟
+        ### 料理步驟
         directions = recipe_searched_soup.select('li[class="step"] div[class="media-body"]')
         directions_list = []
         for i in directions:
@@ -69,27 +84,27 @@ for i in range(1):
         # print("4", type(directions_str))
         # print(directions_str)
 
-        ###文字儲存
-        text_path = folder_path + "/" + str(recipe_num) + "_" + dish_name + ".txt"
-        if os.path.exists(text_path) == False:
-            with open(file = text_path, mode = "w", encoding= 'utf-8') as note:
-                all_str = dish_name + "\n" + ingredients_str + "\n" + quantities_str +  "\n" + directions_str
-                note.write(all_str)
-        else:
-            print("Text " + str(recipe_num) + " already downloaded")
-
-
-        ###圖片
-        img_url = recipe_searched_soup.select('div[class="recipe-picture-frame"] a')[0]["href"]
-        img_html = requests.get(url = img_url, headers = my_headers)
-        img_content = img_html.content
-        img_path = folder_path + "/" + str(recipe_num) + "_" + dish_name + ".jpg"
-
-        if os.path.exists(img_path) == False:
-            with open(file = img_path, mode = "wb") as pic:
-                pic.write(img_content)
-        else:
-            print("Image " + str(recipe_num) + " already downloaded")
+        # ### 文字儲存
+        # text_path = folder_path + "/" + str(recipe_num) + "_" + dish_name + ".txt"
+        # if os.path.exists(text_path) == False:
+        #     with open(file = text_path, mode = "w", encoding= 'utf-8') as note:
+        #         all_str = dish_name + "\n" + ingredients_str + "\n" + quantities_str +  "\n" + directions_str
+        #         note.write(all_str)
+        # else:
+        #     print("Text " + str(recipe_num) + " already downloaded")
+        #
+        #
+        # ### 圖片
+        # img_url = recipe_searched_soup.select('div[class="recipe-picture-frame"] a')[0]["href"]
+        # img_html = requests.get(url = img_url, headers = my_headers)
+        # img_content = img_html.content
+        # img_path = folder_path + "/" + str(recipe_num) + "_" + dish_name + ".jpg"
+        #
+        # if os.path.exists(img_path) == False:
+        #     with open(file = img_path, mode = "wb") as pic:
+        #         pic.write(img_content)
+        # else:
+        #     print("Image " + str(recipe_num) + " already downloaded")
 
         print("Recipe download complete: " + recipe_url)
         print("======================================================================================================")
@@ -99,7 +114,7 @@ for i in range(1):
         print("Error occurred at link: " + recipe_url)
         print(str(err))
 
-        ###紀錄錯誤log
+        ### 紀錄錯誤log
         log_name = "log_" + now_date
         log_path = folder_path + "/" + log_name + ".txt"
 
@@ -109,6 +124,6 @@ for i in range(1):
 
 
 
-    ###接續下一頁
+    ### 接續下一頁
     recipe_num = recipe_num + 1
     # time.sleep(random.randint(2,5))
